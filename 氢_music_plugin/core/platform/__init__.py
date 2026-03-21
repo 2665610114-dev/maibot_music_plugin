a11664f401f048769c5ed4026f37987d13ee21f8
Hydrogen <2665610114@qq.com>
2026-03-21 11:39:18 +0800

    Delete 氢_music_plugin directory

--- 氢_music_plugin/core/platform/__init__.py
+++
@@ -1,9 +0,0 @@
-"""音乐平台模块"""
-from .base import BaseMusicPlayer
-
-# 确保基类先加载，再加载子类以触发 __init_subclass__
-from .ncm import NetEaseMusic
-from .ncm_nodejs import NetEaseMusicNodeJS
-from .txqq import TXQQMusic
-
-__all__ = ["NetEaseMusic", "NetEaseMusicNodeJS", "BaseMusicPlayer", "TXQQMusic"]
